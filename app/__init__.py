a87a202e5650119bb3e852a6ee98b0b9a1426d7a
Tiago M Martins <81379449+TiagoMoraesMartins@users.noreply.github.com>
2022-01-12 22:49:48 -0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -37,8 +37,8 @@ def create_app():
     # Email
     app.config["MAIL_SERVER"] = "smtp.mailtrap.io"
     app.config["MAIL_PORT"] = 587
-    app.config["MAIL_USERNAME"] = "d1dfeade58bfa5"
-    app.config["MAIL_PASSWORD"] = "3409468f9aef90"
+    app.config["MAIL_USERNAME"] = "xxxxxxxxxxxxxx"
+    app.config["MAIL_PASSWORD"] = "xxxxxxxxxxxxxx"
     app.config["MAIL_USE_TLS"] = True
     app.config["MAIL_USE_SSL"] = False
 
